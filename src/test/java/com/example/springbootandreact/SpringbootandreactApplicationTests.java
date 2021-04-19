e750b629d144b65152161cbfec564a2bbcc7dcff
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-04-20 08:32:28 +0900

    second commit

---
+++ src/test/java/com/example/springbootandreact/SpringbootandreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootandreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootandreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
